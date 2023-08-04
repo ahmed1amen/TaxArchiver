f9d5192963f72f89833b68bfee048e88c007bd69
Ahmed Amen <ahmed1amen.g@Gmail.com>
2023-08-04 19:26:33 +0300

    Fix Form LoadAdd project files.

---
+++ TaxArchiver/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TaxArchiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button13 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button15 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button13
+            // 
+            this.button13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button13.BackColor = System.Drawing.Color.Tomato;
+            this.button13.FlatAppearance.BorderSize = 0;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.Location = new System.Drawing.Point(907, 507);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(146, 70);
+            this.button13.TabIndex = 74;
+            this.button13.Text = "Exit";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 77);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(879, 514);
+            this.listView1.TabIndex = 73;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Facture";
+            this.columnHeader1.Width = 83;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Table";
+            this.columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Utilisateur";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Serveur";
+            this.columnHeader4.Width = 105;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Montant";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader5.Width = 109;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "service";
+            this.columnHeader6.Width = 0;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Date";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader7.Width = 100;
+            // 
+            // button15
+            // 
+            this.button15.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button15.BackColor = System.Drawing.Color.SpringGreen;
+            this.button15.FlatAppearance.BorderSize = 0;
+            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(907, 278);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(146, 70);
+            this.button15.TabIndex = 91;
+            this.button15.Text = "Duplicata";
+            this.button15.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button2.Location = new System.Drawing.Point(907, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 70);
+            this.button2.TabIndex = 93;
+            this.button2.Text = "Application Settings";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(180, 16);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(229, 40);
+            this.dateTimePicker1.TabIndex = 94;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.dateTimePicker2.Location = new System.Drawing.Point(476, 16);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(229, 40);
+            this.dateTimePicker2.TabIndex = 95;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(97, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 33);
+            this.label1.TabIndex = 96;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(424, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 33);
+            this.label2.TabIndex = 97;
+            this.label2.Text = "To";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1075, 608);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ArchiveFacture";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+
+
+        private Control _lastActiveControl;
+        private ListViewItem ligne;
+
+        private Button button13;
+
+        private ListView listView1;
+
+        private ColumnHeader columnHeader1;
+
+        private ColumnHeader columnHeader2;
+
+        private ColumnHeader columnHeader3;
+
+        private ColumnHeader columnHeader4;
+
+        private ColumnHeader columnHeader5;
+
+        private ColumnHeader columnHeader6;
+
+        private ColumnHeader columnHeader7;
+
+        private Button button15;
+        private Button button2;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label1;
+        private Label label2;
+    }
+}
+
---
+++ TaxArchiver/Settings.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TaxArchiver
+{
+    public partial class settings : Form
+    {
+        public settings()
+        {
+            InitializeComponent();
+        }
+        private void settings_Load(object sender, EventArgs e)
+        {
+        
+
+            // Create the Configuration object
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+            // List of settings to load
+            string[] settings = new string[] { "server", "port", "database", "username", "password" };
+            TextBox[] textBoxes = new TextBox[] { txt_server, txt_port, txt_database, txt_username, txt_password };
+
+            // Load each setting
+            for (int i = 0; i < settings.Length; i++)
+            {
+                KeyValueConfigurationElement setting = config.AppSettings.Settings[settings[i]];
+
+                if (setting != null)
+                {
+                    // Set the text box value
+                    textBoxes[i].Text = setting.Value;
+                }
+                else
+                {
+                    // Clear the text box if the setting doesn't exist
+                    textBoxes[i].Text = "";
+                }
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            // Create the Configuration object
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+
+            // List of settings to modify
+            string[] settings = new string[] { "server", "port", "database", "username", "password" };
+            TextBox[] textBoxes = new TextBox[] { txt_server, txt_port, txt_database, txt_username, txt_password };
+
+            // Modify each setting
+            for (int i = 0; i < settings.Length; i++)
+            {
+                KeyValueConfigurationElement setting = config.AppSettings.Settings[settings[i]];
+                string textBoxText = textBoxes[i].Text;
+
+                if (setting != null)
+                {
+                    // Update the setting
+                    setting.Value = textBoxText;
+                }
+                else
+                {
+                    // Add the setting
+                    config.AppSettings.Settings.Add(settings[i], textBoxText);
+                }
+            }
+
+            // Save the configuration file
+            config.Save(ConfigurationSaveMode.Modified);
+
+            // Force a reload of the changed section, if needed
+            ConfigurationManager.RefreshSection("appSettings");
+
+            MessageBox.Show("Settings Saved", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            this.Close();
+        }
+    }
+}
